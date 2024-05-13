f60fe7cafb0ad86cfd3a4bc814c70118d90a5f15
Leonardo Rebonatto <leonardo.rebonatto@trafegus.com.br>
2024-05-13 19:29:55 -0300

    Publish app in Firebase hosting, configure CI and useful features.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+interface ProcessEnv {
+  [key: string]: string | undefined;
+}
+
+declare var process: {
+  env: ProcessEnv;
+};
+
 const cherryPickedKeys = [
   "REACT_APP_FIREBASE_API_KEY",
   "REACT_APP_FIREBASE_AUTH_DOMAIN",
   "REACT_APP_FIREBASE_PROJECT_ID",
+  "REACT_APP_FIREBASE_DATABASE_URL",
   "REACT_APP_FIREBASE_STORAGE_BUCKET",
   "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
   "REACT_APP_FIREBASE_APP_ID"
@@ -12,9 +21,9 @@ const cherryPickedKeys = [
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const processEnv = {};
-  cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
+  const env = loadEnv(mode, '', 'REACT_APP_FIREBASE_');
+  const processEnv = {} as ProcessEnv;
+  cherryPickedKeys.forEach(key => processEnv[key] = env[key] as string);
 
   return {
     define: {
@@ -32,4 +41,3 @@ export default defineConfig(({ mode }) => {
     },
   }
 })
-
